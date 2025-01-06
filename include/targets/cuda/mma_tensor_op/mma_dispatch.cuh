254a79b5c6a31998ce4957a36d21eb654cfaddc3
Jiqun Tu <jtu@nvidia.com>
2025-01-06 23:38:42 +0000

    Allow specify MMA types per precision.

--- include/targets/cuda/mma_tensor_op/mma_dispatch.cuh
+++ include/targets/cuda/mma_tensor_op/mma_dispatch.cuh
@@ -51,108 +51,264 @@ namespace quda
     };
 #endif
 
+// QUDA_MULTIGRID_MMA_PROLONGATOR modulo 8
+// - 0 DEFAULT
+// - 1 SIMT
+// - 2 SMMA
+// - 3 1xFP16
+// - 4 3xFP16
+// - 5 1xTF32
+// - 6 3xTF32
+// - 7 3xBF16
+
+#if defined(QUDA_MULTIGRID_MMA_DSLASH)
     template <class T>
-    struct mg_mma_dslash_t {
-#if defined(QUDA_MULTIGRID_MMA_DSLASH_SIMT)
+    struct mg_mma_dslash_t { };
+
+    // @brief half precision specialization
+    template <>
+    struct mg_mma_dslash_t<short> {
+#if ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 1)
       using type = simt::simt_t<float, 8, 4, 2, 2>;
-#elif defined(QUDA_MULTIGRID_MMA_DSLASH_SMMA)
-      using type = typename smma_dispatch<T>::type;
-#elif defined(QUDA_MULTIGRID_MMA_DSLASH_1xFP16)
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 2)
+      using type = typename smma_dispatch<short>::type;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 3)
       using type = hmma_t;
-#elif defined(QUDA_MULTIGRID_MMA_DSLASH_3xFP16)
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 4)
       using type = smma_half_t;
-#elif defined(QUDA_MULTIGRID_MMA_DSLASH_1xTF32)
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 5)
       using type = hmma::hmma_tfloat32_t<4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_DSLASH_3xTF32)
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 6)
       using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_DSLASH_3xBF16)
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 7)
       using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
-#else
+#elif ((QUDA_MULTIGRID_MMA_DSLASH & 7) == 0)
   #if (__COMPUTE_CAPABILITY__ >= 800)
-      using type = typename smma_dispatch<T>::type;
+      using type = typename smma_dispatch<short>::type;
   #else
       using type = simt::simt_t<float, 8, 4, 2, 2>;
   #endif
 #endif
     };
 
-    template <class T>
-    struct mg_mma_prolongator_t {
-#if defined(QUDA_MULTIGRID_MMA_PROLONGATOR_SIMT)
+    // @brief single precision specialization
+    template <>
+    struct mg_mma_dslash_t<float> {
+#if ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 1)
       using type = simt::simt_t<float, 8, 4, 2, 2>;
-#elif defined(QUDA_MULTIGRID_MMA_PROLONGATOR_SMMA)
-      using type = typename smma_dispatch<T>::type;
-#elif defined(QUDA_MULTIGRID_MMA_PROLONGATOR_1xFP16)
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 2)
+      using type = typename smma_dispatch<float>::type;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 3)
+      using type = hmma_t;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 4)
+      using type = smma_half_t;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 5)
+      using type = hmma::hmma_tfloat32_t<4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 6)
+      using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 7)
+      using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_DSLASH >> 3) == 0)
+  #if (__COMPUTE_CAPABILITY__ >= 800)
+      using type = typename smma_dispatch<float>::type;
+  #else
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+  #endif
+#endif
+    };
+#endif
+
+#if defined(QUDA_MULTIGRID_MMA_PROLONGATOR)
+    template <class T>
+    struct mg_mma_prolongator_t { };
+
+    // @brief half precision specialization
+    template <>
+    struct mg_mma_prolongator_t<short> {
+#if ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 1)
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 2)
+      using type = typename smma_dispatch<short>::type;
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 3)
   #if (__COMPUTE_CAPABILITY__ == 700)
-      using type = smma::smma_x_t<mma::half, 8, 1, 1>;
+      using type = hmma::hmma_x_t<16, 8, 8, half, half2>;
   #else
       using type = hmma::hmma_t<16, 8, 8, half, half2>;
   #endif
-#elif defined(QUDA_MULTIGRID_MMA_PROLONGATOR_3xFP16)
-      using type = smma_half_t;
-#elif defined(QUDA_MULTIGRID_MMA_PROLONGATOR_1xTF32)
-      using type = hmma::hmma_tfloat32_t<4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_PROLONGATOR_3xTF32)
-      using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_PROLONGATOR_3xBF16)
-      using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
-#else
-  #if (__COMPUTE_CAPABILITY__ >= 800)
-      using type = typename smma_dispatch<T>::type;
-  #else
-      using type = simt::simt_t<float, 8, 4, 2, 2>;
-  #endif
-#endif
-    };
-
-    template <class T>
-    struct mg_mma_restrictor_t {
-#if defined(QUDA_MULTIGRID_MMA_RESTRICTOR_SIMT)
-      using type = simt::simt_t<float, 8, 4, 2, 2>;
-#elif defined(QUDA_MULTIGRID_MMA_RESTRICTOR_SMMA)
-      using type = typename smma_dispatch<T>::type;
-#elif defined(QUDA_MULTIGRID_MMA_RESTRICTOR_1xFP16)
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 4)
   #if (__COMPUTE_CAPABILITY__ == 700)
       using type = smma::smma_x_t<mma::half, 8, 1, 1>;
   #else
-      using type = hmma::hmma_t<16, 8, 8, half, half2>;
-  #endif
-#elif defined(QUDA_MULTIGRID_MMA_RESTRICTOR_3xFP16)
       using type = smma_half_t;
-#elif defined(QUDA_MULTIGRID_MMA_RESTRICTOR_1xTF32)
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 5)
       using type = hmma::hmma_tfloat32_t<4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_RESTRICTOR_3xTF32)
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 6)
       using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_RESTRICTOR_3xBF16)
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 7)
       using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
-#else
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR & 7) == 0)
   #if (__COMPUTE_CAPABILITY__ >= 800)
-      using type = typename smma_dispatch<T>::type;
+      using type = typename smma_dispatch<short>::type;
   #else
       using type = simt::simt_t<float, 8, 4, 2, 2>;
   #endif
 #endif
     };
 
-    template <class T> struct mg_mma_setup_t {
-#if defined(QUDA_MULTIGRID_MMA_SETUP_SIMT)
+    // @brief single precision specialization
+    template <>
+    struct mg_mma_prolongator_t<float> {
+#if ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 1)
       using type = simt::simt_t<float, 8, 4, 2, 2>;
-#elif defined(QUDA_MULTIGRID_MMA_SETUP_SMMA)
-      using type = typename smma_dispatch<T>::type;
-#elif defined(QUDA_MULTIGRID_MMA_SETUP_1xFP16)
-      using type = hmma_t;
-#elif defined(QUDA_MULTIGRID_MMA_SETUP_3xFP16)
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 2)
+      using type = typename smma_dispatch<float>::type;
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 3)
+  #if (__COMPUTE_CAPABILITY__ == 700)
+      using type = hmma::hmma_x_t<16, 8, 8, half, half2>;
+  #else
+      using type = hmma::hmma_t<16, 8, 8, half, half2>;
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 4)
+  #if (__COMPUTE_CAPABILITY__ == 700)
+      using type = smma::smma_x_t<mma::half, 8, 1, 1>;
+  #else
       using type = smma_half_t;
-#elif defined(QUDA_MULTIGRID_MMA_SETUP_1xTF32)
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 5)
       using type = hmma::hmma_tfloat32_t<4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_SETUP_3xTF32)
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 6)
       using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
-#elif defined(QUDA_MULTIGRID_MMA_SETUP_3xBF16)
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 7)
       using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
-#else
+#elif ((QUDA_MULTIGRID_MMA_PROLONGATOR >> 3) == 0)
+  #if (__COMPUTE_CAPABILITY__ >= 800)
+      using type = typename smma_dispatch<float>::type;
+  #else
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+  #endif
+#endif
+    };
+#endif
+
+#if defined(QUDA_MULTIGRID_MMA_RESTRICTOR)
+    template <class T>
+    struct mg_mma_restrictor_t { };
+
+    // @brief half precision specialization
+    template <>
+    struct mg_mma_restrictor_t<short> {
+#if ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 1)
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 2)
+      using type = typename smma_dispatch<short>::type;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 3)
+  #if (__COMPUTE_CAPABILITY__ == 700)
+      using type = hmma::hmma_x_t<16, 8, 8, half, half2>;
+  #else
+      using type = hmma::hmma_t<16, 8, 8, half, half2>;
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 4)
+  #if (__COMPUTE_CAPABILITY__ == 700)
+      using type = smma::smma_x_t<mma::half, 8, 1, 1>;
+  #else
+      using type = smma_half_t;
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 5)
+      using type = hmma::hmma_tfloat32_t<4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 6)
+      using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 7)
+      using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR & 7) == 0)
+  #if (__COMPUTE_CAPABILITY__ >= 800)
+      using type = typename smma_dispatch<short>::type;
+  #else
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+  #endif
+#endif
+    };
+
+    // @brief single precision specialization
+    template <>
+    struct mg_mma_restrictor_t<float> {
+#if ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 1)
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 2)
+      using type = typename smma_dispatch<float>::type;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 3)
+  #if (__COMPUTE_CAPABILITY__ == 700)
+      using type = hmma::hmma_x_t<16, 8, 8, half, half2>;
+  #else
+      using type = hmma::hmma_t<16, 8, 8, half, half2>;
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 4)
+  #if (__COMPUTE_CAPABILITY__ == 700)
+      using type = smma::smma_x_t<mma::half, 8, 1, 1>;
+  #else
+      using type = smma_half_t;
+  #endif
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 5)
+      using type = hmma::hmma_tfloat32_t<4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 6)
+      using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 7)
+      using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_RESTRICTOR >> 3) == 0)
+  #if (__COMPUTE_CAPABILITY__ >= 800)
+      using type = typename smma_dispatch<float>::type;
+  #else
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+  #endif
+#endif
+    };
+#endif
+
+#if defined(QUDA_MULTIGRID_MMA_SETUP)
+    template <class T> struct mg_mma_setup_t { };
+
+    // @brief half precision specialization
+    template <> struct mg_mma_setup_t<short> {
+#if ((QUDA_MULTIGRID_MMA_SETUP & 7) == 1)
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 2)
+      using type = typename smma_dispatch<short>::type;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 3)
+      using type = hmma_t;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 4)
+      using type = smma_half_t;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 5)
+      using type = hmma::hmma_tfloat32_t<4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 6)
+      using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 7)
+      using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP & 7) == 0)
       using type = hmma_t;
 #endif
     };
+
+    // @brief single precision specialization
+    template <> struct mg_mma_setup_t<float> {
+#if ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 1)
+      using type = simt::simt_t<float, 8, 4, 2, 2>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 2)
+      using type = typename smma_dispatch<float>::type;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 3)
+      using type = hmma_t;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 4)
+      using type = smma_half_t;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 5)
+      using type = hmma::hmma_tfloat32_t<4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 6)
+      using type = smma::smma_t<mma::tfloat32, 4, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 7)
+      using type = smma::smma_t<mma::bfloat16, 8, 1, 1>;
+#elif ((QUDA_MULTIGRID_MMA_SETUP >> 3) == 0)
+      using type = hmma_t;
+#endif
+    };
+#endif
 
   } // namespace mma
 } // namespace quda
